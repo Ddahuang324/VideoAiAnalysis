255895c06e2a55fccd5491233411f1de4322f944
大黄 <2778105141@qq.com>
2026-01-16 23:11:54 +0800

    DemoV1.0

--- scripts/cmake_build_python.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM 获取项目根目录
-set "SCRIPT_DIR=%~dp0"
-set "PROJECT_ROOT=%SCRIPT_DIR%.."
-
-call "D:\Visual Studio\VC\Auxiliary\Build\vcvars64.bat"
-cmake --build "%PROJECT_ROOT%\build" --target python_bindings --config Debug
